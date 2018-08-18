2679acd3cda4ad1465c6499c50473dff6e4aaf2b
Ashish Kumar (JHA) <askumar@microsoft.com>
2018-08-18 23:49:31 +0530

    first commit

---
+++ TestApplication/LoginTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestFramework;
+
+namespace TestApplication
+{
+	[TestClass]
+	public class LoginTests
+	{
+		[TestInitialize]
+		public void Init()
+		{
+			Driver.Initialize(); 
+		}
+
+
+		[TestMethod]
+		public void Admin_User_Can_Login_Test()
+		{
+			LoginPage.GoTo();
+			LoginPage.LoginAs("root").WithPassword("novell").Login();
+			Assert.IsTrue(DashboardPage.IsAt, "Failed to login"); 
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			Driver.Close(); 
+		}
+	}
+}
---
+++ TestFramework/DashboardPage.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework
+{
+	public class DashboardPage
+	{
+		public static bool IsAt
+		{
+			get
+			{
+				var wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+				IReadOnlyCollection<IWebElement> h1s = wait.Until(driver => driver.FindElements(By.TagName("h1")));
+				//var elements = wait.Until(condition =>
+				//{
+				//	try
+				//	{
+				//		var elementToBeDisplayed = Driver.Instance.FindElements(By.TagName("h2"));
+				//		return elementToBeDisplayed; 
+				//	}
+				//	catch (StaleElementReferenceException)
+				//	{
+				//		return false;
+				//	}
+				//	catch (NoSuchElementException)
+				//	{
+				//		return false;
+				//	}
+				//});
+
+
+				//var wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+				//wait.Until(ExpectedConditions.ElementExists((By.TagName("h2")))); 
+				//wait.Until(d => d.SwitchTo().ActiveElement(). GetAttribute("id") == "user_login");
+				//var h2s = Driver.Instance.FindElements(By.TagName("h2"));
+				if (h1s.Count > 0)
+				{
+					//If there is an element with tag=h2 and having text as "Dashboard", then return true, else, return false. 
+					return h1s.ElementAt<IWebElement>(0).Text == "Dashboard"; 
+
+					//return h2s.GetEnumerator().Current.Text == "Dashboard";
+				}
+				else
+				{
+					return false; 
+				}
+			}
+
+		}
+	}
+}
